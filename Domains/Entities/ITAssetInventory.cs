09008e94b02426c0c97167fbd922fbdab1b918b2
Thanh <lnpttor@gmail.com>
2022-05-08 23:29:52 +0700

    add entities

---
+++ Domains/Entities/ITAssetInventory.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReviewEF.Domains.Entities
+{
+    public class ITAssetInventory : BaseEntity
+    {
+        [ForeignKey("ITAssets")]
+        public long AssetId { get; set; }
+        public DateTime InventoryDate { get; set; }
+        public int NumberAssigned { get; set; }
+        public int NumberInStock { get; set; }
+        public string? OtherDetails { get; set; }
+
+        public virtual ITAssets ITAssetsNavigation { get; set; }
+    }
+}
